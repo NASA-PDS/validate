NASA-PDS/validate

commit 2c96ee58e6d7a080fd8fd6195808495c6e8c1755

Updated parent LID check to include ':'
Previous implementation did not ensure exact validity of parent bundle/collection lid.

Updates also include a refactoring to move method to parent abstract class

Resolves #139

src/main/java/gov/nasa/pds/tools/validate/rule/AbstractValidationRule.java
@@ -13,8 +13,10 @@
 // $Id$
 package gov.nasa.pds.tools.validate.rule;
 
+import gov.nasa.pds.tools.label.ExceptionType;
 import gov.nasa.pds.tools.validate.ProblemDefinition;
 import gov.nasa.pds.tools.validate.ProblemListener;
+import gov.nasa.pds.tools.validate.ProblemType;
 import gov.nasa.pds.tools.validate.TargetRegistrar;
 import gov.nasa.pds.tools.validate.ValidationTarget;
 import gov.nasa.pds.tools.validate.ValidationProblem;
@@ -206,6 +208,42 @@ public abstract class AbstractValidationRule implements ValidationRule {
    */
   public final void setCaption(String caption) {
     this.caption = caption;
+  }
+  
+  /**
+   * Verifies that the lid contains the parent bundle/collection lid. Applies to
+   * Bundle and Collection referential integrity (L5.PRP.VA.36)
+   * 
+   * @see gov.nasa.pds.tools.validate.rule.pds4.BundleReferentialIntegrityRule
+   * @see gov.nasa.pds.tools.validate.rule.pds4.CollectionReferentialIntegrityRule
+   * 
+   * @param lid
+   * @param parentLid
+   * @param status
+   * @param url
+   */
+  protected void verifyLidPrefix (String lid, String parentLid, String status,
+          URL url) {
+    if (!status.equalsIgnoreCase("Primary")) {
+        return;
+    }
+    if (!lid.startsWith(parentLid + ":")) {
+      getListener().addProblem(new ValidationProblem(
+          new ProblemDefinition(
+              ExceptionType.ERROR,
+              ProblemType.MISSING_PARENT_PREFIX,
+              "Member LID " + lid + " does not begin with required parent LID " +
+              parentLid),
+          url));
+    } else {
+        getListener().addProblem(new ValidationProblem(
+            new ProblemDefinition(
+                ExceptionType.INFO,
+                ProblemType.PARENT_PREFIX_FOUND,
+                "Member LID " + lid + " begins with required parent LID " +
+                parentLid),
+            url));
+    }
   }
 
 }

src/main/java/gov/nasa/pds/tools/validate/rule/pds4/BundleReferentialIntegrityRule.java
@@ -234,28 +234,4 @@ public class BundleReferentialIntegrityRule extends AbstractValidationRule {
     return results;
   }
   
-  private void verifyLidPrefix (String lid, String parentLid, String status,
-      URL bundle) {
-	if (!status.equalsIgnoreCase("Primary")) {
-		return;
-	}
-	if (!lid.startsWith(parentLid)) {
-      getListener().addProblem(new ValidationProblem(
-	      new ProblemDefinition(
-	          ExceptionType.ERROR,
-	          ProblemType.MISSING_PARENT_PREFIX,
-	          "Member LID " + lid + " does not begin with parent LID " +
-	          parentLid),
-	      bundle));
-	} else {
-		getListener().addProblem(new ValidationProblem(
-			new ProblemDefinition(
-			    ExceptionType.INFO,
-			    ProblemType.PARENT_PREFIX_FOUND,
-			    "Member LID " + lid + " begins with parent LID " +
-			    parentLid),
-			bundle));
-	}
-  }
-  
 }

src/main/java/gov/nasa/pds/tools/validate/rule/pds4/CollectionReferentialIntegrityRule.java
@@ -217,29 +217,4 @@ public class CollectionReferentialIntegrityRule extends AbstractValidationRule {
     return results;
   }
   
-  private void verifyLidPrefix (String lid, String parentLid, String status,
-      URL collection) {
-    if (!status.equalsIgnoreCase("P")) {
-    	  //System.out.println("Non-primary member: " + lid + ", " + status);
-	  return;
-    }
-	if (!lid.startsWith(parentLid)) {
-	  getListener().addProblem(new ValidationProblem(
-		  new ProblemDefinition(
-		       ExceptionType.ERROR,
-		       ProblemType.MISSING_PARENT_PREFIX,
-		       "Member LID " + lid + " does not begin with parent LID " +
-		       parentLid),
-		   collection));
-	} else {
-	  getListener().addProblem(new ValidationProblem(
-		  new ProblemDefinition(
-			   ExceptionType.INFO,
-			   ProblemType.PARENT_PREFIX_FOUND,
-			   "Member LID " + lid + " begins with parent LID " +
-			   parentLid),
-		    collection));
-	}
-  }
-  
 }

pom.xml
@@ -37,7 +37,7 @@ POSSIBILITY OF SUCH DAMAGE.
 
   <groupId>gov.nasa.pds</groupId>
   <artifactId>validate</artifactId>
-  <version>1.18.0-SNAPSHOT-SNAPSHOT</version>
+  <version>1.18.0-SNAPSHOT</version>
   <packaging>jar</packaging>
 
   <name>${project.groupId}:${project.artifactId}</name>

commit 5c7df3b0584ad53961ab7596ed135898983b7ac0

Fix bug with parent LID integrity checking for collections
Bug introduced with commit 2c96ee58e6d7a080fd8fd6195808495c6e8c1755, this adds additional checks to allow for collection level integrity checking, not just bundle level.

Resolves #146

src/main/java/gov/nasa/pds/tools/validate/rule/AbstractValidationRule.java
@@ -224,26 +224,24 @@ public abstract class AbstractValidationRule implements ValidationRule {
    */
   protected void verifyLidPrefix (String lid, String parentLid, String status,
           URL url) {
-    if (!status.equalsIgnoreCase("Primary")) {
-        return;
-    }
-    if (!lid.startsWith(parentLid + ":")) {
-      getListener().addProblem(new ValidationProblem(
-          new ProblemDefinition(
-              ExceptionType.ERROR,
-              ProblemType.MISSING_PARENT_PREFIX,
-              "Member LID " + lid + " does not begin with required parent LID " +
-              parentLid),
-          url));
-    } else {
-        getListener().addProblem(new ValidationProblem(
-            new ProblemDefinition(
-                ExceptionType.INFO,
-                ProblemType.PARENT_PREFIX_FOUND,
-                "Member LID " + lid + " begins with required parent LID " +
-                parentLid),
-            url));
+    if (status.equalsIgnoreCase("Primary") || status.equalsIgnoreCase("P")) {
+        if (!lid.startsWith(parentLid + ":")) {
+          getListener().addProblem(new ValidationProblem(
+              new ProblemDefinition(
+                  ExceptionType.ERROR,
+                  ProblemType.MISSING_PARENT_PREFIX,
+                  "Member LID " + lid + " does not begin with required parent LID " +
+                  parentLid),
+              url));
+        } else {
+            getListener().addProblem(new ValidationProblem(
+                new ProblemDefinition(
+                    ExceptionType.INFO,
+                    ProblemType.PARENT_PREFIX_FOUND,
+                    "Member LID " + lid + " begins with required parent LID " +
+                    parentLid),
+                url));
+        }
     }
   }
-
 }

src/main/java/gov/nasa/pds/tools/validate/rule/pds4/BundleReferentialIntegrityRule.java
@@ -145,8 +145,8 @@ public class BundleReferentialIntegrityRule extends AbstractValidationRule {
                   + "any product within the given target."), 
               bundle));
         } else if (matchingMembers.size() == 1) {
-        	  String parentLid = extractor.getValueFromDoc(LOGICAL_IDENTIFIER);
-          verifyLidPrefix(id.getLid(), parentLid, memberStatus, bundle);
+          String parentLid = extractor.getValueFromDoc(LOGICAL_IDENTIFIER);
+          super.verifyLidPrefix(id.getLid(), parentLid, memberStatus, bundle);
           getListener().addProblem(new ValidationProblem(new ProblemDefinition(
               ExceptionType.INFO,
               ProblemType.MEMBER_FOUND,
@@ -156,7 +156,7 @@ public class BundleReferentialIntegrityRule extends AbstractValidationRule {
               bundle));
         } else if (matchingMembers.size() > 1) {
           String parentLid = extractor.getValueFromDoc(LOGICAL_IDENTIFIER);
-          verifyLidPrefix(id.getLid(), parentLid, memberStatus, bundle);
+          super.verifyLidPrefix(id.getLid(), parentLid, memberStatus, bundle);
           ExceptionType exceptionType = ExceptionType.ERROR;
           if (!id.hasVersion()) {
             Map<String, List<String>> matchingIds = 

src/main/java/gov/nasa/pds/tools/validate/rule/pds4/CollectionReferentialIntegrityRule.java
@@ -125,7 +125,7 @@ public class CollectionReferentialIntegrityRule extends AbstractValidationRule {
                           + "any product within the given target."), 
                       collection));
             } else if (matchingMembers.size() == 1) {
-              verifyLidPrefix(id.getLid(), this.lid, entry.getMemberStatus(),
+              super.verifyLidPrefix(id.getLid(), this.lid, entry.getMemberStatus(),
             	      collection);
               getListener().addProblem(new ValidationProblem(
                   new ProblemDefinition(ExceptionType.INFO,
@@ -135,7 +135,7 @@ public class CollectionReferentialIntegrityRule extends AbstractValidationRule {
                           + matchingMembers.get(0).getValue()), 
                       collection));
             } else if (matchingMembers.size() > 1) {
-              verifyLidPrefix(id.getLid(), this.lid, entry.getMemberStatus(),
+              super.verifyLidPrefix(id.getLid(), this.lid, entry.getMemberStatus(),
             	      collection);
               ExceptionType exceptionType = ExceptionType.ERROR;
               if (!id.hasVersion()) {