bc4c7569a461d5b51d48e46e5e5584a12f216df5
Jordan Padams <jordan.h.padams@jpl.nasa.gov>
2020-01-29 17:00:43 -0800

    Add build script for updating context

---
+++ build/update_context.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Hacky script to build validate, generate new context products, and push to git
+#
+set -x
+rm -fr validate-*/
+mvn clean package -DskipTests
+tar -xvzf target/validate-*-bin.tar.gz
+validate-*/bin/validate -u
+cp validate-*/resources/registered_context_products.json src/main/resources/util/
+git add src/main/resources/util/
+git commit -m "Update context products"
+set +x
