861964ddaeedad13022c85c312cac4130b5fa69f
Jordan Padams <jordan.h.padams@jpl.nasa.gov>
2020-03-26 17:49:43 -0700

    Add pds4-jparser upgrade to pre-build

--- build/pre-build.sh
+++ build/pre-build.sh
@@ -2,6 +2,19 @@
 #
 # Hacky script to build validate, generate new context products, and push to git
 #
+
+usage() {
+    echo "$(basename $0) <new-pds4-jparser-version>"
+    echo
+    exit 1
+}
+
+if [ $# -ne 1 ]; then
+    usage
+fi
+
+version=$1
+
 set -x
 rm -fr validate-*/
 mvn clean package -DskipTests
@@ -10,5 +23,7 @@ validate-*/bin/validate -u
 cp validate-*/resources/registered_context_products.json src/main/resources/util/
 git add src/main/resources/util/
 git commit -m "Update context products for release"
+
+mvn versions:set-property -Dproperty=pds4-jparser.version -DnewVersion=$version
 
 set +x
