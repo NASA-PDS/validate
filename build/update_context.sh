4a1a11eb44e423c8711cb0ec7730b2703ed0a7dc
Jordan Padams <jordan.h.padams@jpl.nasa.gov>
2020-09-16 15:52:52 -0700

    fix bug in tar function

--- build/update_context.sh
+++ build/update_context.sh
@@ -7,7 +7,7 @@ set -x
 outdir=/tmp
 rm -fr $outdir/validate-*
 mvn clean package -DskipTests
-tar -xvzf target/validate-*-bin.tar.gz $outdir
+tar -xvzf target/validate-*-bin.tar.gz -C $outdir
 $outdir/validate-*/bin/validate -u
 cp $outdir/validate-*/resources/registered_context_products.json src/main/resources/util/
 set +x
