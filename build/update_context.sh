4d5904d298061640b70477a3fab91ec38f3b5ab5
Jordan Padams <jordan.h.padams@jpl.nasa.gov>
2020-09-16 15:44:34 -0700

    Update context config and CI/CD

--- build/update_context.sh
+++ build/update_context.sh
@@ -4,21 +4,11 @@
 #
 
 set -x
-rm -fr validate-*/
+outdir=/tmp/validate
+rm -fr $outdir
 mvn clean package -DskipTests
-tar -xvzf target/validate-*-bin.tar.gz
-validate-*/bin/validate -u
-cp validate-*/resources/registered_context_products.json src/main/resources/util/
-
-set +x
-git config --local user.email "$1"
-git config --local user.name "$2"
-git config --local github.token $3
-
-set -x
-git add src/main/resources/util/
-git commit -m "Update config with latest context product metadata"
-
-git push origin master
+tar -xvzf target/validate-*-bin.tar.gz $outdir
+$outdir/bin/validate -u
+cp $outdir/validate/resources/registered_context_products.json src/main/resources/util/
 set +x
 
