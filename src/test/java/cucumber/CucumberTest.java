0db7188d2a29aaf20254d10b2737875f25d25e3a
Jordan Padams <jordan.h.padams@jpl.nasa.gov>
2021-02-03 12:35:50 -0800

    Add cucumber test runner

---
+++ src/test/java/cucumber/CucumberTest.java
@@ -0,0 +1,16 @@
+package cucumber;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty", "html:target/cucumber.html"},
+    features = "src/test/resources/features/",
+    glue = "cucumber")
+public class CucumberTest {
+    
+    public CucumberTest() { }
+
+}
