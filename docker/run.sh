e6790f455b0c9bffb5c30a3a0a68fc75b7041ef9
Ramesh Maddegoda <94033485+ramesh-maddegoda@users.noreply.github.com>
2022-12-21 17:15:08 -0800

    UPDATE to pass default arguments as an array and use ENTRYPOINT in Dockerfile instead of CMD
    Refer to task NASA-PDS/validate#556

--- docker/run.sh
+++ docker/run.sh
@@ -1,16 +1,52 @@
 #!/bin/sh
 
+# Copyright 2022, California Institute of Technology ("Caltech").
+# U.S. Government sponsorship acknowledged.
+#
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+# * Redistributions must reproduce the above copyright notice, this list of
+# conditions and the following disclaimer in the documentation and/or other
+# materials provided with the distribution.
+# * Neither the name of Caltech nor its operating division, the Jet Propulsion
+# Laboratory, nor the names of its contributors may be used to endorse or
+# promote products derived from this software without specific prior written
+# permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
 # Update following environment variable to match with your environment
 export PDS_BUNDLE_PATH= #<ADD PDS BUNDLE PATH>
 export CHECKSUM_MANIFEST_FILE_NAME=urn-nasa-pds-insight_rad.md5
-export VALIDATE_REPORT_PATH=/tmp/
+export VALIDATE_REPORT_PATH=/tmp
+export VALIDATE_REPORT_FILE_NAME=validate-report.txt
 
-export DEFAULT_ARGS="/tmp/pds -M /tmp/pds/"${CHECKSUM_MANIFEST_FILE_NAME}" -r /tmp/validate-report/validate-report.txt -R pds4.bundle"
+# Provide default arguments as an array
+DEFAULT_ARGS=(/tmp/pds -M /tmp/pds/${CHECKSUM_MANIFEST_FILE_NAME} -r /tmp/validate-report/${VALIDATE_REPORT_FILE_NAME} -R pds4.bundle)
 
+echo "Executing: validate ${DEFAULT_ARGS[*]}"
+
+# Execute validate docker container with default arguments
 docker container run --name validate \
                  --rm \
                  --volume "${PDS_BUNDLE_PATH}":/tmp/pds \
                  --volume "${PDS_BUNDLE_PATH}"/"${CHECKSUM_MANIFEST_FILE_NAME}":/tmp/pds/"${CHECKSUM_MANIFEST_FILE_NAME}" \
                  --volume "${VALIDATE_REPORT_PATH}":/tmp/validate-report/ \
-                 --env DEFAULT_ARGS="${DEFAULT_ARGS}" \
-                 nasapds/validate           
+                 nasapds/validate "${DEFAULT_ARGS[@]}"
+
+echo "The validate report is available at: ${VALIDATE_REPORT_PATH}/${VALIDATE_REPORT_FILE_NAME}"
