0ea547ad18d426dd25c3da63806e88442b8d999c
Al Niessner <Al.Niessner@xxx.xxx>
2023-02-14 08:19:14 -0800

    work aonther ticket

---
+++ src/main/java/gov/nasa/pds/validate/ri/AuthInformation.java
@@ -0,0 +1,96 @@
+package gov.nasa.pds.validate.ri;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.Scanner;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class AuthInformation {
+  final public static AuthInformation NO_AUTH = new AuthInformation(false, "", "" ,"");
+  final private boolean trustSelfSigned;
+  final private String password;
+  final private String url;
+  final private String username;
+
+  private AuthInformation (boolean tss, String pwd, String un, String url) {
+    this.password = pwd;
+    this.trustSelfSigned = tss;
+    this.url = url;
+    this.username = un;
+  }
+
+  public static AuthInformation buildFrom (String filename)
+      throws IOException,ParserConfigurationException,SAXException {
+    boolean tss;
+    File file = new File(filename);
+    Scanner textReader;
+    String line = null, pwd, un, url;
+
+    if (filename == null || filename.length() == 0) return NO_AUTH;
+    if (!file.exists())
+      throw new IOException("Filename '" + filename + "' does not exist");
+
+    // Get the first non-comment line
+    textReader = new Scanner(file);
+    while (textReader.hasNext() && line == null) {
+      line = textReader.nextLine().strip();
+      if (line.charAt(0) == '#') line = null;
+    }
+    textReader.close();
+    
+    // Determine which file processing to use
+    if (line.startsWith("<?xml version=\"1.0\" encoding=\"UTF-8\"?>")) { // XML
+      // <registry url="http://localhost:9200" index="registry" auth="/path/to/auth.cfg" />
+      DocumentBuilder builder = DocumentBuilderFactory.newDefaultInstance().newDocumentBuilder();
+      Document document = builder.parse(file);
+      NodeList registries = document.getElementsByTagName("registry");
+
+      if (registries.getLength() != 1)
+        throw new SAXException("There should be one and only registry tag in the harvest config file but found " + registries.getLength());
+      if (registries.item(0).getAttributes().getNamedItem("auth") == null)
+        throw new SAXException("Requires an authorization file or 'auth' attribute on <registry>.");
+
+      filename = registries.item(0).getAttributes().getNamedItem("auth").getNodeValue();
+      url = registries.item(0).getAttributes().getNamedItem("url").getNodeValue();
+      url += ("/" + registries.item(0).getAttributes().getNamedItem("index").getNodeValue());
+    }
+    else { // java property
+      FileInputStream input = new FileInputStream(file);
+      Properties properties = new Properties();
+      properties.load(input);
+      url = properties.getProperty("url");
+      filename = properties.getProperty("credentials");
+      input.close();
+    }
+    
+    // Get credentials
+    FileInputStream input = new FileInputStream(filename);
+    Properties properties = new Properties();
+    properties.load(input);
+    pwd = properties.getProperty("password");
+    tss = Boolean.valueOf(properties.getProperty("trust.self-signed", "false"));
+    un = properties.getProperty("user");
+    input.close();
+    return new AuthInformation(tss, pwd, un, url);
+  }
+
+  public String getPassword() {
+    return password;
+  }
+  public boolean getTrustSelfSigned() {
+    return trustSelfSigned;
+  }
+  public String getUsername() {
+    return username;
+  }
+  public String getUrl() {
+    return url;
+  }
+}
--- src/main/java/gov/nasa/pds/validate/ri/CommandLineInterface.java
+++ src/main/java/gov/nasa/pds/validate/ri/CommandLineInterface.java
@@ -1,5 +1,9 @@
 package gov.nasa.pds.validate.ri;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.HelpFormatter;
@@ -13,27 +17,20 @@ public class CommandLineInterface {
   public CommandLineInterface() {
     super();
     this.opts = new Options();
-    this.opts.addOption(Option.builder("a")
+    this.opts.addOption(Option.builder("A")
         .argName("auth-file")
-        .desc("a java property file with the URL and credential content to have full (all product states) read-only access to the registry API")
+        .desc("file with the URL and credential content to have full (all product states) read-only access to the registry API")
         .hasArg(true)
         .longOpt("auth-api")
         .numberOfArgs(1)
         .optionalArg(true)
         .build());
-    this.opts.addOption(Option.builder("A")
+    this.opts.addOption(Option.builder("a")
         .argName("auth-file")
-        .desc("a java property file with the URL and credential content to have full, direct read-only access to the search DB")
+        .desc("file with the URL and credential content to have full, direct read-only access to the search DB")
         .hasArg(true)
         .longOpt("auth-search")
         .numberOfArgs(1)
-        .optionalArg(true)
-        .build());
-    this.opts.addOption(Option.builder("c")
-        .argName("config-file")
-        .desc("an XML file used with harvest ")
-        .hasArg(true)
-        .longOpt("config-harvest")
         .optionalArg(true)
         .build());
     this.opts.addOption(Option.builder("h")
@@ -44,7 +41,7 @@ public class CommandLineInterface {
         .build());
     this.opts.addOption(Option.builder("l")
         .argName("lidvid")
-        .desc("an XML file used with harvest ")
+        .desc("the complete LIDVID to start doing reference checks and will be walked down to the product level if it does not start there")
         .hasArg(true)
         .longOpt("lidvid")
         .optionalArg(true)
@@ -53,9 +50,9 @@ public class CommandLineInterface {
 
   private void help() {
     new HelpFormatter().printHelp("ValidateReferenceIntegrity",
-        "Checks the search DB that all references exist. If the api-auth is provided, then it will also check that the registry API also finds all the references. The lidvid and harvest config file cannot both be given. When the harvest config is given, the auth-search is ignored. For lidvid and config-harvest, multiple values can be given using a comma like 'fileA.xml,fileB.xml'.",
+        "Checks the search DB that all references exist. If the api-auth is provided, then it will also check that the registry API also finds all the references. For lidvid, multiple values can be given using a comma like 'urn:foo::1.0,urn:bar::2.0'.",
         opts,
-        "An auth-file is a text file of the Java property format with two variables: 'url' and 'credentials'. The 'url' property should be the complete URL like 'https://localhost:9876/root/endpoint' and 'credentials' a java property file with the user name, password, and other credential information as those used by harvest.",
+        "An auth-file is either a text file of the Java property format with two variables: 'url' and 'credentials'. The 'url' property should be the complete base URL like 'https://localhost:9876/base' and 'credentials' a java property file with the user name, password, and other credential information as that used by harvest. Or it is an XML text file used by harvest with <registry> containing the 'auth' attribute.",
         true);
   }
 
@@ -68,18 +65,26 @@ public class CommandLineInterface {
         return;
       }
       
-      if (cl.hasOption("c") && cl.hasOption("l"))
-        throw new ParseException("Cannot process both a lidvid and harvest config file at the same time.");
-      if (!cl.hasOption("c") && !cl.hasOption("l"))
-        throw new ParseException("Must provide either a lidvid or a harvest config.");
-      if (cl.hasOption("l") && !cl.hasOption("A"))
-        throw new ParseException("Must provide the auth-search when providing lidvid(s).");
+      if (!cl.hasOption("a"))
+        throw new ParseException("Must provide search authorization information.");
+      if (!cl.hasOption("l"))
+        throw new ParseException("Must provide at least one LIDVID as a starting point.");
+      if (!cl.hasOption("A"))
+        log.warning ("Only the DB will be checked because no registry authorization was given");
 
-      System.out.println(cl.getOptionValue("l"));
+      this.temp(Arrays.asList(cl.getOptionValue("lidvid").split(",")),
+          AuthInformation.buildFrom(cl.getOptionValue("auth-api", "")),
+          AuthInformation.buildFrom(cl.getOptionValue("auth-search")));
     }
     catch (ParseException pe) {
       System.err.println("[ERROR] " + pe.getMessage());
       this.help();
     }
+  }
+  
+  public void temp(List<String> lidvids, AuthInformation registry, AuthInformation search) {
+    System.out.println ("lidvids:  " + lidvids);
+    System.out.println ("registry: " + registry);
+    System.out.println ("search:   " + search);
   }
 }
