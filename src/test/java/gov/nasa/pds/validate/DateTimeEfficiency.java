7eacab7d9f3919a89d79a1b8a0cfd62a029a578b
Al Niessner <Al.Niessner@xxx.xxx>
2023-04-05 11:44:24 -0700

    first stage of efficiency improvement

---
+++ src/test/java/gov/nasa/pds/validate/DateTimeEfficiency.java
@@ -0,0 +1,20 @@
+package gov.nasa.pds.validate;
+
+import gov.nasa.pds.label.object.FieldType;
+import gov.nasa.pds.tools.validate.rule.pds4.DateTimeValidator;
+
+public class DateTimeEfficiency {
+
+  public static void main(String[] args) {
+    try {
+      long t0 = System.currentTimeMillis();
+      for (int i=0 ; i < 1000000 ; i++) {
+        DateTimeValidator.isValid(FieldType.ASCII_DATE_TIME_DOY_UTC, "2023-120T10:23:55.123456Z");
+      }
+      System.out.println("Processing time: " + (System.currentTimeMillis() - t0)/1000. + " seconds");
+    } catch (Exception e) {
+      System.err.println("Complete and utter failure.");
+    }
+  }
+
+}
