c4f99dd4f115d2d8f9eab4109dd7386b861dcb8b
Al Niessner <Al.Niessner@xxx.xxx>
2024-10-31 08:54:01 -0700

    updated POM and code
    With updated POM and code, the jaxb stuff can reliably be resolved fixing many of the transient problems with the validate regression testing. Now lets see if the github actions agree

--- src/main/java/gov/nasa/pds/validate/util/Namespace.java
+++ src/main/java/gov/nasa/pds/validate/util/Namespace.java
@@ -38,10 +38,10 @@
 
 package gov.nasa.pds.validate.util;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlType;
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlAttribute;
+import jakarta.xml.bind.annotation.XmlType;
 
 /**
  * <p>
