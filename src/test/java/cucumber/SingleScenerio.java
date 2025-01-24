f0d17501378b9cb3d315bc1e22031a8b04748be2
Al Niessner <Al.Niessner@xxx.xxx>
2025-01-24 14:24:18 -0800

    update some helper java

--- src/test/java/cucumber/SingleScenerio.java
+++ src/test/java/cucumber/SingleScenerio.java
@@ -1,16 +1,15 @@
-package gov.nasa.pds.validate;
+package cucumber;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import javax.xml.transform.TransformerConfigurationException;
-import cucumber.StepDefs;
+import gov.nasa.pds.validate.ValidateLauncher;
 import gov.nasa.pds.validate.constants.TestConstants;
 
-public class RunFeatureScenerioAsValidateFromCLI {
-  public static void main(String[] args) throws IOException, TransformerConfigurationException {
+public class SingleScenerio {
+  public static void main(String[] args) throws NumberFormatException, IOException {
     if (args.length > 2 || args.length == 0) {
       System.out.println ("usage: <issue number> <subtest>");
       System.out.println ("  <issue number> is the first column in the feature file like 1066");
@@ -18,7 +17,6 @@ public class RunFeatureScenerioAsValidateFromCLI {
       return;
     }
 
-    ArrayList<String> validate_args = new ArrayList<String>();
     for (File file : Paths.get(TestConstants.TEST_DATA_DIR, "features").toFile().listFiles((dir, name) -> name.endsWith(".feature"))) {
       for (String line : Files.readAllLines(file.toPath())) {
         line = line.strip();
@@ -34,28 +32,26 @@ public class RunFeatureScenerioAsValidateFromCLI {
               return;
             }
             if (args.length == 2 && !args[1].equals(scenerio[2].strip())) continue;
-            StepDefs helper = new StepDefs();
-            helper.an_and(
+            StepDefs engine = new StepDefs();
+            System.out.println("an_and");
+            engine.an_and(
                 Integer.valueOf(scenerio[1].strip()),
                 args.length == 1 ? null : Integer.valueOf(scenerio[2].strip()),
                 scenerio[3].strip().substring(1, scenerio[3].strip().length()-1));
-            validate_args.addAll(helper.resolveArgumentStrings(scenerio[4].strip().substring(1,scenerio[4].strip().length()-1), false));
-            break;
+            System.out.println("execute_validate");
+            engine.execute_validate (scenerio[4].strip().substring(1,scenerio[4].strip().length()-1));
+            System.out.println ("compare_to_the");
+            engine.compare_to_the(scenerio[5].strip().isBlank() ? null :
+              scenerio[5].strip().substring(1, scenerio[5].strip().length()-1));
+            System.out.println ("success");
+            return;
           }
         }
       }
-      if (validate_args.size() > 0) break;
     }
-    if (validate_args.size() == 0) {
-      System.out.print ("Could not find issue number " + args[0]);
-      if (args.length == 2) System.out.print (" and subtest " + args[1]);
-      System.out.println();
-      return;
-    }
-    System.out.println ("Same as: validate " + String.join(" ", validate_args));
-    if (System.getProperty("resources.home") == null) {
-      System.setProperty("resources.home", TestConstants.RESOURCES_DIR);
-    }
-    ValidateLauncher.main(validate_args.toArray(new String[0]));
+    System.out.print ("Could not find issue number " + args[0]);
+    if (args.length == 2) System.out.print (" and subtest " + args[1]);
+    System.out.println();
+    return;
   }
 }
