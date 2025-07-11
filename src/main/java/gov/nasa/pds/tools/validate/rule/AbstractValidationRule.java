4220295b64051ec602d7f6882300c9d2bdab6c02
Jesse Stone <jstone@psi.edu>
2025-07-11 11:03:50 -0700

    added timing information for all rules

--- src/main/java/gov/nasa/pds/tools/validate/rule/AbstractValidationRule.java
+++ src/main/java/gov/nasa/pds/tools/validate/rule/AbstractValidationRule.java
@@ -58,10 +58,21 @@ public abstract class AbstractValidationRule implements ValidationRule {
     if (isApplicable(getTarget().toString())) {
       // Run each annotated validation test.
       for (Method m : getClass().getMethods()) {
+
         Annotation a = m.getAnnotation(ValidationTest.class);
         if (a != null) {
+          long t0 = System.currentTimeMillis();
+
           LOG.debug("AbstractValidationRule:execute: m,a {},{}", m, a);
           m.invoke(this, new Object[0]);
+
+          long t1 = System.currentTimeMillis();
+
+          if (isDebugLogLevel() && (t1-t0 > 1000)) {
+            System.out.println(
+                    "\nDEBUG  [" + ProblemType.TIMING_METRICS.getKey() + "]  " + System.currentTimeMillis()
+                            + " :: " + getTarget().getFile() + " :: " + m.getDeclaringClass().getName() + ":" + m.getName() + " in " + (t1 - t0) + " ms\n");
+          }
         }
       }
     }
