fd095275e76ae527b5c2b8379a171af471179f41
Jordan Padams <jordan.h.padams@jpl.nasa.gov>
2025-08-06 17:17:04 -0700

    Add missing Collection dependency

--- src/main/java/gov/nasa/pds/tools/util/ReferentialIntegrityUtil.java
+++ src/main/java/gov/nasa/pds/tools/util/ReferentialIntegrityUtil.java
@@ -16,6 +16,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
