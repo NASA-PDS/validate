d1449c48a89b691f0d3b77623dd559ee3f6aac8a
Jordan Padams <jordan.h.padams@jpl.nasa.gov>
2025-11-04 12:54:45 -0800

    Update to remove junit 5 warnings

--- src/test/java/cucumber/CucumberTest.java
+++ src/test/java/cucumber/CucumberTest.java
@@ -1,12 +1,12 @@
 package cucumber;
 
 import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
 @IncludeEngines("cucumber")
-
+@SelectClasspathResource("features")
 public class CucumberTest {
-  public CucumberTest() {}
 }
 
